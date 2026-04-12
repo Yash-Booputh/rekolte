3ed57471d5e00ab2a631740d529f43ebc90a39b8
Yash-Booputh <yashvinbooputh@gmail.com>
2026-04-12 23:22:45 +0400

    Updates

---
+++ assets/appState-legacy-BrJVO7Nt.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-C270jTWP.js"],function(e,t){"use strict";var r;return{setters:[e=>{r=e.r}],execute:function(){e("m",r(0))}}});
---
+++ assets/p-CneGxKsZ-Bx-oOPJ0.js
@@ -0,0 +1,3 @@
+import{W as s,P as r,Q as a,R as i,S as m}from"./index-BRv18axh.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const c=()=>{const t=window;t.addEventListener("statusTap",()=>{s(()=>{const n=document.elementFromPoint(t.innerWidth/2,t.innerHeight/2);if(!n)return;const e=r(n);e&&new Promise(o=>a(e,o)).then(()=>{i(async()=>{e.style.setProperty("--overflow","hidden"),await m(e,300),e.style.removeProperty("--overflow")})})})})};export{c as startStatusTap};
