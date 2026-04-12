ee54ace25af47e7481e85177f44cf7da05948597
Yash-Booputh <yashvinbooputh@gmail.com>
2026-04-12 22:28:30 +0400

    Updates

--- assets/appState-BwW1JB3G.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-CqwiqW1Y.js";const r=t(0);export{r as m};
---
+++ assets/appState-legacy-BPjlrFaz.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-DESlLBVR.js"],function(e,t){"use strict";var r;return{setters:[e=>{r=e.r}],execute:function(){e("m",r(0))}}});
